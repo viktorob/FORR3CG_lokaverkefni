4def04687926af4d8183a1a1371c6c9be7db1eaa
Viktor Bjarkason <viktorbj21@tskoli.is>
2023-02-21 09:20:15 +0000

    BunadarListi.cpp og h bætt við

---
+++ BunadarListi.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class BunadarListi() {
+    private:
+        int audkenni;
+        int verdmaeti;
+        char stadsetning[3];
+        
+};
